f93790df2983db7f76d49cecfa68929dafbfac36
Alexandre Gauthier <alexandre230@msn.com>
2016-02-05 11:12:18 +0100

    Add patch  + always use git

--- buildOIIO.sh
+++ buildOIIO.sh
@@ -5,39 +5,38 @@
 # USE_XCODE=1 (optional): (OSX Only) Builds using xcodebuild. The binaries will be symlinked to the Xcode build directory
 # and the actual headers will not be installed
 # MKJOBS (optional): Number of threads for make
-# USE_TAR=1 (optional): if set, this will use the selected OIIO_TAR, otherwise this will fallback on using git and the specified GIT_BRANCH
-# OIIO_VERSION="X.Y.ZW" (optional): the version number of oiio release to use when USE_TAR=1. The archive must be on the remote server already
-# GIT_BRANCH (optional): select git branch if using git
+# GIT_BRANCH (optional): select git branch or tag
 # CONFIG=(debug,release) (required)
 # NO_TOOLS=1:(optional) do not build oiio tools
-#Usage: MKJOBS=8 NO_TOOLS=1 USE_TAR=1 CONFIG=debug ./buildOIIO
-#With Xcode: NO_CLEAN=1 USE_XCODE=1 CONFIG=debug sh buildOIIO.sh
+# DST_DIR=... (required): Where to deploy the library
+#
+#
+#Usage: MKJOBS=8 NO_TOOLS=1 GIT_BRANCH=tags/Release-1.5.23 CONFIG=debug DST_DIR=. ./buildOIIO
+#With Xcode: NO_CLEAN=1 USE_XCODE=1 GIT_BRANCH=master CONFIG=debug DST_DIR=. ./buildOIIO.sh
 
 set -x 
 CWD=$(pwd)
 
-DEFAULT_OIIO_VERSION=1.5.23
+DEFAULT_GIT_BRANCH=tags/Release-1.5.23
 
 if [ -z "$OIIO_VERSION" ]; then
     OIIO_VERSION=$DEFAULT_OIIO_VERSION
 fi
 
-###### To be customized
+if [ -z "$DST_DIR" ]; then
+    ###### To be customized
+    DST_DIR=/Users/alexandre/development/CustomBuilds
+    ######
+fi
 
-#To be set if USE_TAR=1 is specified
-OIIO_TAR=oiio-Release-$OIIO_VERSION.tar.gz
-
-#Where to download OIIO_TAR when USE_TAR=1
-REPO_URL=http://downloads.natron.fr/Third_Party_Sources/
-
-#Where to install binaries
-DST_DIR=/Users/alexandre/development/CustomBuilds
-######
+if [ ! -d "$DST_DIR" ]; then
+    echo "$DST_DIR: Specified DST_DIR does not exist."
+    exit 1
+fi
 
 PATCH_DIR=$CWD/patches
 
 OIIO_GIT=https://github.com/lgritz/oiio.git
-DEFAULT_GIT_BRANCH=master
 
 
 if [ "$CONFIG" = "debug" ]; then
@@ -73,16 +72,8 @@ if [ -z "$NO_CLEAN" ]; then
 fi
 
 if [ ! -d "oiio-src" ]; then
-    if [ "$USE_TAR" != "1" ]; then
-        echo "Using git repository $OIIO_GIT"
-        git clone $OIIO_GIT oiio-src
-    else
-        echo "Using tar archive ${REPO_URL}${OIIO_TAR}"
-        wget ${REPO_URL}${OIIO_TAR} || exit 1
-        tar xf $OIIO_TAR || exit 1
-        rm $OIIO_TAR || exit 1
-        mv oiio-* oiio-src || exit 1
-    fi
+    echo "Using git repository $OIIO_GIT"
+    git clone $OIIO_GIT oiio-src
 fi
 
 cd oiio-src || exit 1
@@ -91,18 +82,17 @@ if [ -z "$NO_CLEAN" ]; then
     rm -rf build
 fi
 
-if [ "$USE_TAR" != "1" ]; then
-    git checkout $GIT_BRANCH || exit 1
-else
-    if [[ "$OIIO_VERSION" = 1.5.* ]]; then
+git checkout $GIT_BRANCH
+
+if [ ! -d build ]; then
+
+    if [[ "$GIT_BRANCH" = *-1.5.* ]]; then
         patch -p1 -i $PATCH_DIR/oiio-1.5.22-exrthreads.patch || exit 1
-    elif [[ "$OIIO_VERSION" = 1.6.* ]]; then
+        patch -p1 -i $PATCH_DIR/oiio-1.5.23-checkmaxmem.patch || exit 1
+    elif [[ "$GIT_BRANCH" = *-1.6.* ]]; then
         patch -p1 -i $PATCH_DIR/oiio-sha1.patch || exit 1
         patch -p1 -i $PATCH_DIR/oiio-x86intrin.patch || exit 1
     fi
-fi
-
-if [ ! -d build ]; then
     mkdir build
     cd build
     cmake -DUSE_QT=0 -DBOOST_ROOT=/opt/Natron-1.0 -DUSE_TBB=0 -DUSE_PYTHON=0 -DUSE_FIELD3D=0 -DUSE_FFMPEG=0 -DUSE_OPENJPEG=0 -DUSE_OCIO=1 -DUSE_OPENCV=0 -DUSE_OPENSSL=0 -DUSE_FREETYPE=1 -DUSE_GIF=1 -DUSE_LIBRAW=1 -DSTOP_ON_WARNING=0 $CMAKE_OIIO_TOOLS -DCMAKE_INSTALL_PREFIX="" ${CMAKE_CONFIG} ${XCODE_EXTRA} .. || exit 1
